d5ff66eb97609e55dcaaab99c93f432ccc0824e9
Pdzly <34943569+Pdzly@users.noreply.github.com>
2023-07-19 21:35:08 +0200

    added example

--- src/config.example.ts
+++ src/config.example.ts
@@ -2,28 +2,28 @@ import IConfig from "./models/iConfig";
 
 export const config: IConfig = {
   fetchInterval: {
-    posts: 1000 * 30,
-    comments: 1000 * 30,
+    posts: 1000 * 15,
+    comments: 1000 * 20,
     reports: 1000 * 60,
     mentions: 1000 * 15,
   },
   communityConfig: [
     {
-      communities: ["nostupidquestions"],
+      communities: ["support", "moderators"],
       config: {
-        discordTeam: ["710213572898193428", "466017970158698497", "512511895820042240", "555572537426444288", "403272094227365889", "1129840347338518550", "431794187562778656"],
+        discordTeam: ["1130506618388349059"],
         logs: {
           discord: {
             enabled: true,
-            logChannel: "1129862373986009188",
+            logChannel: "1131208030235672656",
             logGuild: "1128644575276318801",
-            comments: { enabled: true, threadId: "1130863254080606248" },
-            posts: { enabled: true, threadId: "1130863319658549330" },
-            reports: { enabled: true, threadId: "1130863368903864330" },
+            comments: { enabled: true, threadId: "1131293634893975623" },
+            posts: { enabled: true, threadId: "1131293260237766808" },
+            reports: { enabled: true, threadId: "1131293522021077043" },
             profanity: {
               enabled: false,
-              channel: "1129862373986009188",
-              appendTextToLogMessage: "<@&1129839222040313907> Profanity detected!"
+              channel: "932286006156222497",
+              appendTextToLogMessage: "<@&1038182121572929586> Profanity detected!"
             }
           },
         },
@@ -32,7 +32,7 @@ export const config: IConfig = {
   ],
   logs: {
     discord: {
-      logChannel: "1129862373986009188",
+      logChannel: "1131208030235672656",
       logGuild: "1128644575276318801",
       enabled: true,
       comments: { enabled: true },
